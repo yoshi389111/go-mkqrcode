70e5ba0b6fa6c800640eaf4f6fdfc7ceedb106ef
sato yoshiyuki <yoshi.389111@gmail.com>
2020-05-17 14:40:06 +0900

    :sparkles: add margin option

--- mkqrcode.go
+++ mkqrcode.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
 	"image/color"
 	"image/png"
 	"log"
@@ -14,10 +15,15 @@ import (
 
 func main() {
 	outFile := flag.String("o", "", "output file name")
+	margin := flag.Int("m", 4, "margin of QR-code")
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
 		flag.Usage()
+		os.Exit(1)
+	}
+	if *margin < 0 {
+		fmt.Fprintln(os.Stderr, "margin is negative")
 		os.Exit(1)
 	}
 
@@ -29,7 +35,7 @@ func main() {
 	}
 
 	if *outFile != "" {
-		err = outputQrCode(qrCode, 200, *outFile)
+		err = outputQrCode(qrCode, *margin, 200, *outFile)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -37,14 +43,31 @@ func main() {
 	} else {
 		black := "\033[40m  \033[0m"
 		white := "\033[47m  \033[0m"
-		printQrCode(qrCode, black, white)
+		printQrCode(qrCode, *margin, black, white)
 	}
 }
 
-func outputQrCode(qrCode barcode.Barcode, size int, fileName string) error {
-	qrCode, err := barcode.Scale(qrCode, size, size)
-	if err != nil {
-		return err
+func outputQrCode(qrCode barcode.Barcode, margin, size int, fileName string) error {
+	rect := qrCode.Bounds()
+	cells := rect.Max.X + margin*2
+	if size < cells {
+		return fmt.Errorf("size is too small. required %d <= size", cells)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			cx := x*cells/size - margin
+			cy := y*cells/size - margin
+			if rect.Min.X <= cx && cx < rect.Max.X &&
+				rect.Min.Y <= cy && cy < rect.Max.Y &&
+				qrCode.At(cx, cy) == color.Black {
+
+				img.Set(x, y, color.Black)
+			} else {
+				img.Set(x, y, color.White)
+			}
+		}
 	}
 
 	file, err := os.Create(fileName)
@@ -53,15 +76,18 @@ func outputQrCode(qrCode barcode.Barcode, size int, fileName string) error {
 	}
 	defer file.Close()
 
-	err = png.Encode(file, qrCode)
+	err = png.Encode(file, img)
 	return err
 }
 
-func printQrCode(qrCode barcode.Barcode, black, white string) {
+func printQrCode(qrCode barcode.Barcode, margin int, black, white string) {
 	rect := qrCode.Bounds()
-	for y := rect.Min.Y; y < rect.Max.Y; y++ {
-		for x := rect.Min.X; x < rect.Max.X; x++ {
-			if qrCode.At(x, y) == color.Black {
+	for y := rect.Min.Y - margin; y < rect.Max.Y+margin; y++ {
+		for x := rect.Min.X - margin; x < rect.Max.X+margin; x++ {
+			if rect.Min.X <= x && x < rect.Max.X &&
+				rect.Min.Y <= y && y < rect.Max.Y &&
+				qrCode.At(x, y) == color.Black {
+
 				fmt.Print(black)
 			} else {
 				fmt.Print(white)
